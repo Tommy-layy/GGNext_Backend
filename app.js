4883988d7a44fb4c02a1ea8802f5765167329474
Tommy Le <LeTran.Tommy@gmail.com>
2022-09-07 12:04:08 -0500

    Forgot to redo app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
+const app = express()
+
+const AuthRouter = require('./routes/AuthRouter')
+const PORT = process.env.PORT || 3001
+
+app.use(cors())
+app.use(express.json())
+app.use(logger('dev'))
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => res.json({ message: 'Server Works' }))
+app.use('/auth', AuthRouter)
+app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
