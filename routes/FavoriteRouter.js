938d53f2d48ccbdf3a5f601da59e847083eac8f1
Tommy Le <LeTran.Tommy@gmail.com>
2022-09-09 11:36:39 -0500

    Game Route Changes

--- routes/FavoriteRouter.js
+++ routes/FavoriteRouter.js
@@ -5,3 +5,5 @@ Router.get('/', controller.getFavorite)
 Router.get('/:user_id', controller.getFavoriteByUser)
 Router.get('/games/:favorite_id', controller.getGamesFromFavorite)
 Router.post('/addgame/:favorite_id/:game_id', controller.addGamesToFavorite)
+
+module.exports = Router
--- routes/GameRouter.js
+++ routes/GameRouter.js
@@ -1,1 +1,5 @@
 const Router = require('express').Router()
+const Router = require('../controllers/GameController')
+
+Router.get('/', controller.addGame)
+Router.delete('/:game_id', controller.deleteGame)
